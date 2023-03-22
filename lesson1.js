142aa0afecf714eb30d195e7f3a51de8a38410f0
kavithabatchu12 <52601081+kavithabatchu12@users.noreply.github.com>
2023-03-22 22:17:00 +1030

    first lessons 1 and 2

---
+++ lesson1.js
@@ -0,0 +1,21 @@
+console.log("Hellow world")
+//variables
+var firstname="john"
+let lastname="smith"
+console.log(firstname)
+ var age,dateofBirth,sex
+ age = "5"
+ sex = "Male"
+ console.log(age)
+ age = "6"
+ console.log(age)
+ //constants
+ const occupation ="enjineer"
+
+ console.log(occupation)
+//Data types
+var middleName = "David" //string is in double quotes
+var ageofBrother = 25 //number
+var isHemarried = false //boolean is used in conditions
+var yerasInMarriage = null //null
+var numberOfCars = undefined //
---
+++ lesson2.js
@@ -0,0 +1,18 @@
+var price=50
+var product="cup"
+var printtomessage = "the price is"+product+"is"+price+"dollars"
+var printmsg = 'the price is ${product} is ${price} is'
+console.log(printtomessage)
+console.log(printmsg)
+var custome = {
+    fname: "kavitha",
+    lname: "batchu"
+}
+console.log(custome.fname)
+console.log(custome.lname)
+var age =10>=14
+console.log(age)
+//strict comprison is checking the datatype aswell
+var x='1'
+console.log(x=='1')
+console.log(x==='1')
